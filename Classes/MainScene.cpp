e63254d24acc82fc3a941578f70b432b2fdd2767
eitabilly <longlongago003@gmail.com>
2016-07-14 19:59:45 +0900

    StoneStrike スタート

---
+++ Classes/MainScene.cpp
@@ -0,0 +1,9 @@
+//
+//  MainScene.cpp
+//  StoneStrike
+//
+//  Created by 石田栄太 on 2016/07/14.
+//
+//
+
+#include "MainScene.hpp"
---
+++ Classes/MainScene.hpp
@@ -0,0 +1,14 @@
+//
+//  MainScene.hpp
+//  StoneStrike
+//
+//  Created by 石田栄太 on 2016/07/14.
+//
+//
+
+#ifndef MainScene_hpp
+#define MainScene_hpp
+
+#include <stdio.h>
+
+#endif /* MainScene_hpp */
